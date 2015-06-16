d61b04a2b0e3fa2cb13cb63d7eb94cd15cd05080
Kevin Xu <1kevinxu1@gmail.com>
2015-06-16 09:33:51 -0700

    Begin working on ajax requests for userportal

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,13 @@ class SessionsController < ApplicationController
   before_action :logged_in?, only: [:create]
 
   def create
-    @user = User.find_by_credentials(params[:user][:email],
-                                     params[:user][:password])
+    @user = User.find_by_credentials(login_params)
     if @user
       sign_in!(@user)
       redirect_to root_url
     else
       flash.now[:errors] = ["Wrong email/password combination"]
-      render :new
+      render json: @user.errors.full_messages
     end
   end
 
@@ -18,4 +17,9 @@ class SessionsController < ApplicationController
     redirect_to new_session_url
   end
 
+  private
+
+  def login_params
+    params.require(:user).permit(:email, :password)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
 
   def create
+    debugger;
     @user = User.new(user_params)
     if @user.save
       sign_in!(@user)
       redirect_to root_url
     else
       flash.now[:errors] = @user.errors.full_messages
-      render 'sessions/new'
+      render json: @user.errors.full_messages
     end
   end
 
